f069479bcfff019781a8484d331f823b91b26fbc
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-27 12:47:40 -0300

    Creacion de variable con la sentencia var

---
+++ Ejercicio09-Crear-variables-con-la-sentencia-var.js
@@ -0,0 +1,27 @@
+// Ejercicio 9: Crear variable con la sentencia var
+
+// Declaramos la variable numero
+// El tipo de datos de una variable puede variar.
+// Se puede declarar varias variables en la misma linea.
+
+var numero;
+
+// asignamos un valor
+numero = 7;
+
+// mostramos por consola el valor de la variable
+console.log(numero);
+
+// ver el typo de datos de la variable
+console.log(typeof(numero));
+
+// asignamos un valor string
+numero = '7';
+
+// ver el typo de datos de la variable
+console.log(typeof(numero));
+
+var x, y;
+
+var a = 1, b = 'JavaScript';
+
