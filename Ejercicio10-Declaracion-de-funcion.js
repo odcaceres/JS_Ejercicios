682f3d3b8be405565ad5742b1ecdfadd8dc5483a
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-27 12:56:55 -0300

    Declaracion de funcion

---
+++ Ejercicio10-Declaracion-de-funcion.js
@@ -0,0 +1,12 @@
+// Ejercicio 10: Declarar una funcion usando la instruccion function.
+
+function sumar(a, b){
+    var suma = a + b;
+
+    return suma;
+}
+
+console.log(sumar(2,3));
+console.log(sumar(7,3));
+console.log(sumar(11,3));
+console.log(sumar(2,13));
